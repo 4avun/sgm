e7bfa8b2c30b72805cce0c2f31bff72ed8f09681
4avun <savaoffgd@gmail.com>
2023-08-31 13:25:57 +0300

    something

--- lib/calculator/temp/damage_token_example.dart
+++
@@ -1,3 +0,0 @@
-final attackToken = <String, dynamic> {
-  'id': ''
-};
--- lib/small_apps/xo_game.dart
+++ lib/small_apps/xo_game.dart
@@ -1,4 +1,6 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 class XOgame extends StatefulWidget {
   const XOgame({super.key});
@@ -8,9 +10,9 @@ class XOgame extends StatefulWidget {
 }
 
 class _XOgameState extends State<XOgame> {
-  Widget _getBlock(Block block) => Positioned(
-      left: block.row * 200,
-      top: block.column * 200,
+  Widget _getField(Field field) => Positioned(
+      left: field.row * 200,
+      top: field.column * 200,
       child: Container(
         width: 180,
         height: 180,
@@ -39,16 +41,61 @@ class _XOgameState extends State<XOgame> {
   }
 }
 
-class Block {
-  const Block({required this.column, required this.row, required this.value});
+class Field extends Equatable {
+  const Field({required this.column, required this.row, required this.value});
   final int row;
   final int column;
   final String value;
 
-  Block copyWith({
+  @override
+  List<Object?> get props => [row, column, value];
+
+  Field copyWith({
     String? value,
   }) {
-    return Block(
+    return Field(
         column: this.column, row: this.row, value: value ?? this.value);
   }
+}
+
+class XOState extends Equatable {
+  XOState({this.fields = const [], required this.settings});
+  final List<Field> fields;
+  final FieldsSettings settings;
+
+  @override
+  List<Object?> get props => [fields, settings];
+}
+
+class XOBloc {
+  XOBloc();
+  final _controller = BehaviorSubject<XOState>();
+}
+
+class FieldsSettings extends Equatable {
+  FieldsSettings()
+      : this.column = 3,
+        this.dimentions = 600,
+        this.row = 3;
+
+  FieldsSettings._({
+    required this.column,
+    required this.dimentions,
+    required this.row,
+  });
+
+  FieldsSettings copyWith(int? row, int? column, int dimentions) {
+    return FieldsSettings._(
+      column: this.column,
+      dimentions: this.dimentions,
+      row: this.row,
+    );
+  }
+
+  final int row;
+  final int column;
+  final int dimentions;
+
+  @override
+  List<Object?> get props => [row, column, dimentions];
 }
